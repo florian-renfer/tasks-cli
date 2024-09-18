1cb1d4e0da560290196a513c44e179f32e38caae
Pyxels <39232833+Pyxels@users.noreply.github.com>
2024-09-18 20:09:18 +0200

    chore(flake): add go dev shell

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Tasks CLI";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+          ];
+        };
+      };
+    };
+}
